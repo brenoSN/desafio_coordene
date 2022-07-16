4ee270792faedb2fa711050b65ba0cc7d8e850ac
brenoSN <99511303+brenoSN@users.noreply.github.com>
2022-07-15 23:39:27 -0300

    Initial user interface

--- code/src/components/BestTransport.vue
+++ code/src/components/BestTransport.vue
@@ -1,15 +1,50 @@
 <template>
+
   <div class="title">
     <b-navbar toggleable="lg" type="dark" variant="info">
       <b-navbar-brand class="ml-2">
+        <img class="logo" src = "../assets/logo.png">
         <b>{{ appName }}</b>
       </b-navbar-brand>
     </b-navbar>
+    <div class = "content">
+    <p id="highlightedText"> Como é o frete que você precisa?</p>
+    
+    <fieldset class = "field">
+      <label> Selecione o destino do frete </label>
+      <br>
+      <select name = "cities" id = "city">
+
+      </select>
+      <br>
+    </fieldset>
+    <div class = "field">
+      <label for = "weight"> Peso </label>
+      <br>
+      <input type = "number" step = "0.01" id = "weight" placeholder = "Insira aqui o peso da carga em Kg">
+    </div>
+    <button class = "button" id = "analyze"> Analisar</button>
+    <div class = "result">
+      <p id="highlightedText"> Estas são as melhores alternativas de frete que encontramos para você</p>
+     </div>
   </div>
+
+  </div>
+
 
 </template>
 
 <script>
+
+function buttonClicked(){
+    console.log("Button clicked");
+}
+
+window.onload=function(){
+  var btn = document.getElementById("analyze");
+  btn.addEventListener("click", buttonClicked, true);
+}
+
 import {
   BNavbar,
   BNavbarBrand,
@@ -22,31 +57,126 @@ export default {
   },
   data() {
     const appName = ''
-
+    const api_data = []
     return {
       appName,
+      api_data,
     }
   },
   created() {
     // Implemente aqui o GET dos dados da API REST
     // para que isso ocorra na inicialização da pagina
-    this.appName = 'Melhor Frete'
+    this.appName = 'MELHOR FRETE'
   },
+  
   methods: {
     // Implemente aqui os metodos utilizados na pagina
     methodFoo() {
       console.log(this.appName)
-    },
+    }
+
   },
+  
 }
+
+
 </script>
 
 <style scoped>
+
+body{
+    background-color: #F0F8FF;
+    font-family: sans-serif;
+    font-size: 1em;
+    color: #59429d;
+    margin-left: 20%;
+    margin-right: 20%;
+    margin-top: 2%;
+    justify-content: center;
+}
+
 .title .navbar {
   background-color: #00aca6 !important;
+  
 }
 
 .title .navbar-brand {
   margin-left: 20px;
 }
+
+.logo {
+    z-index: 9999;
+    height: 5rem;
+    width: 5rem; 
+    padding: 8px;
+    top:10px; 
+    left:10px;
+}
+
+#highlightedText {
+    font-family: sans-serif;
+    font-weight: bold;
+    color: #000000;
+}
+
+.field {
+  margin-top: 0.75cm;
+}
+
+.content {
+  justify-content: center;
+  margin-left: 40%;
+  padding-top: 1cm;
+}
+
+
+label {
+  font-family: sans-serif;
+  font-size: 1em;
+  color: #000000;
+}
+
+input, select, button {
+    font-family: sans-serif;
+    font-size: 1em;
+    color: #000000;
+    border-radius: 5px;
+}
+
+input, select {
+  margin: 5px;
+  width: 10cm;
+  padding: 2px;
+}
+
+/* Elemento de classe "botao" */
+.button {
+    font-size: 1.2em;
+    background: #00aca6;
+    border: 0;
+    margin-bottom: 1em;
+    margin-top: 0.85cm;
+    color: #ffffff;
+    padding: 0.2em 0.6em;
+    box-shadow: 2px 2px 2px rgba(0,0,0,0.2);
+    text-shadow: 1px 1px 1px rgba(0,0,0,0.5);
+}
+
+/* Elemento de classe "botao" com o estado da pseudoclasse "hover" */
+.button:hover {
+    background: #029792;
+    box-shadow: inset 2px 2px 2px rgba(0,0,0,0.2);
+    text-shadow: none;
+}
+
+/* Elementos de classe botão e de tag <select> */
+.button, select{
+    cursor: pointer;
+}
+
+.result {
+  margin-top: 10%;
+  text-align: left 
+}
+
 </style>
